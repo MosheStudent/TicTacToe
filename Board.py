758d78e116e728577e06f8b4119711acbd69096c
MosheStudent <moshebekritsky@orotyehuda.org.il>
2025-05-25 15:59:38 +0300

    Add files via upload

--- Board.py
+++ Board.py
@@ -3,7 +3,13 @@ class Board:
         self.board = ["1","2","3","4","5","6","7","8","9"]
 
     def display(self):
-        return f"\n{self.board[0] | self.board[1] | self.board[2]}\n--+---+--\n{self.board[3] | self.board[4] | self.board[5]}\n--+---+--\n{self.board[6] | self.board[7] | self.board[8]}"
+        return (
+            f"\n{self.board[0]} | {self.board[1]} | {self.board[2]}"
+            f"\n--+---+--"
+            f"\n{self.board[3]} | {self.board[4]} | {self.board[5]}"
+            f"\n--+---+--"
+            f"\n{self.board[6]} | {self.board[7]} | {self.board[8]}"
+        )
     
     def update(self, position, marker):
         self.board[position] = marker
