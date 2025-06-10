f27964da796e8bb51846c8dea0294fd529223d32
MosheStudent <moshebekritsky@orotyehuda.org.il>
2025-06-10 10:12:31 +0300

    fixed class error

--- GameRoom.py
+++ GameRoom.py
@@ -1,47 +1,56 @@
-def send_board_to_players(self):
-    board_state = self.board.display()
-    # Send "Your Move" message only to the current player, but always include latest board for both
-    for i, (player_socket, _) in enumerate(self.players):
-        if i == self.current_player:
-            msg = board_state + "\nYour Move:"
-        else:
-            msg = board_state
-        player_socket.send(encrypt(msg))
+from Board import Board
+from cypher import encrypt, decrypt
 
-def handle_game(self):
-    game_over = False
+class GameRoom:
+    def __init__(self, player1_socket, player2_socket):
+        self.board = Board()
+        self.players = [(player1_socket, "x"), (player2_socket, "o")]   
+        self.current_player = 0
 
-    while not game_over:
-        current_socket, current_marker = self.players[self.current_player]
+    def send_board_to_players(self):
+        board_state = self.board.display()
 
-        self.send_board_to_players()
+        for player_socket, _ in self.players:
+            player_socket.send(bytes(encrypt(board_state), "utf-8"))
 
-        move = decrypt(current_socket.recv(1024))
+    def handle_game(self):
+        game_over = False
 
-        # Validate move
-        try:
-            move = int(move) - 1
-            if self.board.board[move] not in ["x", "o"]:
-                self.board.update(move, current_marker)
+        while not game_over:
+            current_socket, current_marker = self.players[self.current_player]
+            opponent_socket, _ = self.players[(self.current_player + 1) % 2]
+
+            self.send_board_to_players()
+
+            current_socket.send(bytes(encrypt("Your Move: "), "utf-8"))
+            move = decrypt(current_socket.recv(1024).decode("utf-8"))
+
+            #validate move
+            if move.isdigit() and int(move) in range(1,10):
+                move = int(move) - 1
+
+                if self.board.board[move] not in ["x", "o"]:
+                    self.board.update(move, current_marker)
+
+                    if self.board.is_winner(current_marker):
+                        self.send_board_to_players()
+                        current_socket.send(bytes(f"Player {current_marker} Wins!", "utf-8"))
+                        opponent_socket.send(bytes(f"Player {current_marker} Wins!", "utf-8"))
+
+                        game_over = True
+                    
+                    elif self.board.is_draw():
+                        self.send_board_to_players()
+                        current_socket.send(bytes("It's a draw!", "utf-8"))
+                        opponent_socket.send(bytes("It's a draw!", "utf-8"))
+
+                        game_over = True
+                    
+                    else:
+                        self.current_player = (self.current_player + 1) % 2
+                
+                else:
+                    current_socket.send(bytes("Invalid move, try again", "utf-8"))
+                
             else:
-                continue  # Invalid move, ask again
-        except Exception:
-            continue  # Invalid input, ask again
-
-        # Check for win/draw
-        if self.board.is_winner(current_marker):
-            msg = f"Player {current_marker.upper()} Wins!"
-            for player_socket, _ in self.players:
-                player_socket.send(encrypt(self.board.display() + "\n" + msg))
-            game_over = True
-            self.save_game_result(msg)
-        elif self.board.is_draw():
-            msg = "It's a draw!"
-            for player_socket, _ in self.players:
-                player_socket.send(encrypt(self.board.display() + "\n" + msg))
-            game_over = True
-            self.save_game_result(msg)
-        else:
-            self.current_player = (self.current_player + 1) % 2
-
-    # Optionally, close sockets here if you want to end the session
+                current_socket.send(bytes("Invalid move, try again", "utf-8"))
